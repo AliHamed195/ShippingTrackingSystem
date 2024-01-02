dd797b7473776f986321c3a2e92c1fda556b3752
alihamed <alihamed.cg@gmail.com>
2024-01-02 22:22:47 +0200

    edit in models

--- ShippingTrackingSystem/Models/Order.cs
+++ ShippingTrackingSystem/Models/Order.cs
@@ -1,4 +1,6 @@
-﻿using ShippingTrackingSystem.Enum;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using ShippingTrackingSystem.Enum;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShippingTrackingSystem.Models
 {
@@ -7,6 +9,10 @@ namespace ShippingTrackingSystem.Models
         public int Id { get; set; }
         public int TotalAmount { get; set; }
         public OrderStatus Status { get; set; }
+
+        [Display(Name = "Shipping Tracking Number")]
+        [ValidateNever]
+        public string TrackingNumber { get; set; }
         public DateTime CreatedOn { get; set; } = DateTime.Now;
         public bool IsDeleted { get; set; } = false;
         public DateTime? EstimatedDeliveryDate { get; set; } = DateTime.Now.AddDays(3);
