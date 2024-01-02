ec07baf4b2ef77ce0c4f5590682785671dd5c166
alihamed <alihamed.cg@gmail.com>
2024-01-02 23:14:39 +0200

    start with user controller

--- ShippingTrackingSystem/Controllers/UserController.cs
+++ ShippingTrackingSystem/Controllers/UserController.cs
@@ -1,10 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShippingTrackingSystem.BackEnd.Interfaces;
 
 namespace ShippingTrackingSystem.Controllers
 {
+    [Route("User")]
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly IAccountRepository _accountRepository;
+
+        public UserController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        [HttpGet("All")]
+        public IActionResult AllUsers()
+        {
+            return View();
+        }
+
+        [HttpGet("Create")]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpGet("Edit")]
+        public IActionResult EditUser()
+        {
+            return View();
+        }
+
+        [HttpGet("Details")]
+        public IActionResult UserDetails()
+        {
+            return View();
+        }
+
+        [HttpGet("Delete")]
+        public IActionResult DeleteUser()
         {
             return View();
         }
