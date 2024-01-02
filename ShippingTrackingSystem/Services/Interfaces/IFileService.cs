55fabc71c37aec6c7c34ec2e6a09e878c2f85a71
alihamed <alihamed.cg@gmail.com>
2024-01-02 22:34:16 +0200

    creating IFileService

---
+++ ShippingTrackingSystem/Services/Interfaces/IFileService.cs
@@ -0,0 +1,8 @@
+﻿namespace ShippingTrackingSystem.Services.Interfaces
+{
+    public interface IFileService
+    {
+        Task<string?> AddFileAsync(IFormFile UploadedFile);
+        bool DeleteFileAsync(string FilePath);
+    }
+}
---
+++ ShippingTrackingSystem/Services/Repository/FileService.cs
@@ -0,0 +1,61 @@
+﻿using ShippingTrackingSystem.Services.Interfaces;
+
+namespace ShippingTrackingSystem.Services.Repository
+{
+    public class FileService : IFileService
+    {
+        public async Task<string?> AddFileAsync(IFormFile UploadedFile)
+        {
+            try
+            {
+                if (UploadedFile == null || UploadedFile.Length == 0)
+                {
+                    return null;
+                }
+
+                var fileName = Path.GetFileNameWithoutExtension(UploadedFile.FileName);
+                var extension = Path.GetExtension(UploadedFile.FileName);
+                var guid = Guid.NewGuid().ToString();
+                var fileModelName = $"{guid}_{DateTime.Now:yyyyMMddHHmmss}_{fileName}_{extension}";
+                var path = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot/Images", fileModelName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await UploadedFile.CopyToAsync(stream);
+                }
+
+                return $"/Images/{fileModelName}";
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public bool DeleteFileAsync(string FilePath)
+        {
+            try
+            {
+                // Check if the filePath starts with '/' and remove it
+                if (FilePath.StartsWith("/"))
+                {
+                    FilePath = FilePath.TrimStart('/');
+                }
+
+                var fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", FilePath);
+
+                if (File.Exists(fullPath))
+                {
+                    File.Delete(fullPath);
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
