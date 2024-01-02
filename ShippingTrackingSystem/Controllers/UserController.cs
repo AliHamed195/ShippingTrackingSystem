10393359d8cf1bc3d037fad660bb86c5e0e08dbc
alihamed <alihamed.cg@gmail.com>
2024-01-02 23:31:33 +0200

    creating the basic implementation for the user controller but need to be tested

--- ShippingTrackingSystem/Controllers/UserController.cs
+++ ShippingTrackingSystem/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShippingTrackingSystem.BackEnd.Interfaces;
+using ShippingTrackingSystem.Models;
 
 namespace ShippingTrackingSystem.Controllers
 {
@@ -7,16 +8,27 @@ namespace ShippingTrackingSystem.Controllers
     public class UserController : Controller
     {
         private readonly IAccountRepository _accountRepository;
+        private readonly IConfiguration _configuration;
 
-        public UserController(IAccountRepository accountRepository)
+        public UserController(IAccountRepository accountRepository, IConfiguration configuration)
         {
             _accountRepository = accountRepository;
+            _configuration = configuration;
         }
 
         [HttpGet("All")]
-        public IActionResult AllUsers()
+        public async Task<IActionResult> AllUsers()
         {
-            return View();
+            try
+            {
+                var users = await _accountRepository.GetAllUsersAsync();
+                return View(users);
+            }
+            catch (Exception)
+            {
+                ViewBag.ErrorMessage = "An error occurred while processing your request. Please try again later.";
+                return View(Enumerable.Empty<ApplicationUser>());
+            }
         }
 
         [HttpGet("Create")]
@@ -25,22 +37,107 @@ namespace ShippingTrackingSystem.Controllers
             return View();
         }
 
-        [HttpGet("Edit")]
-        public IActionResult EditUser()
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateUser(ApplicationUser newUser)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    string defaultPassword = _configuration["DefaultPassword"];
+                    var user = await _accountRepository.CreateUserAsync(newUser, defaultPassword);
+
+                    if (user is not null)
+                    {
+                        return RedirectToAction(nameof(AllUsers));
+                    }
+
+                    ModelState.AddModelError(string.Empty, "An error occurred while creating the user.");
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError(string.Empty, "An error occurred while creating the user.");
+                }
+            }
+
+            return View(newUser);
         }
 
-        [HttpGet("Details")]
-        public IActionResult UserDetails()
+        [HttpGet("Edit/{id}")]
+        public async Task<IActionResult> EditUser(string id)
         {
-            return View();
+            if (string.IsNullOrEmpty(id))
+            {
+                return RedirectToAction(nameof(AllUsers)); 
+            }
+
+            try
+            {
+                var user = await _accountRepository.GetUserByIdAsync(id);
+                if (user is null)
+                {
+                    return RedirectToAction(nameof(AllUsers));
+                }
+
+                return View(user);
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError(string.Empty, "An error occurred while getting the user.");
+                return View(new ApplicationUser());
+            }
         }
 
-        [HttpGet("Delete")]
-        public IActionResult DeleteUser()
+        [HttpGet("Details/{id}")]
+        public async Task<IActionResult> UserDetails(string id)
         {
-            return View();
+            if (string.IsNullOrEmpty(id))
+            {
+                return RedirectToAction(nameof(AllUsers));
+            }
+
+            try
+            {
+                var user = await _accountRepository.GetUserByIdAsync(id);
+                if (user is null)
+                {
+                    return RedirectToAction(nameof(AllUsers));
+                }
+
+                return View(user);
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError(string.Empty, "An error occurred while getting the user.");
+                return View(new ApplicationUser());
+            }
+        }
+
+
+        [HttpGet("Delete/{id}")]
+        public async Task<IActionResult> DeleteUser(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return Json(new { success = false, message = "User ID is null or empty." });
+            }
+
+            try
+            {
+                var result = await _accountRepository.DeleteUserAsync(id);
+                if (result)
+                {
+                    return Json(new { success = true, message = "User successfully deleted." });
+                }
+                else
+                {
+                    return Json(new { success = false, message = "User not found." });
+                }
+            }
+            catch (Exception)
+            {
+                return Json(new { success = false, message = "An error occurred while deleting the user." });
+            }
         }
     }
 }
